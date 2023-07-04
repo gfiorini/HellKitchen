faa8cd07d9fdbe36ab810a992190cbd1cab26f50
Gian <gianluca.fiorinigmail.com>
2023-07-04 23:39:31 +0200

    trash counter

---
+++ Assets/Scripts/TrashCounter.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashCounter : BaseCounter
+{
+    public override void Interact(Player player) {
+        if (player.GetKitchenObject() != null){
+            player.GetKitchenObject().DestroySelf();
+        }
+    }
+
+    
+}
+
+
+
+
