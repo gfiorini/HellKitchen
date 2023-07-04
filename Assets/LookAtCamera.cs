7affb7c888ae29f0eb4ba1c70d8eca3a726e6d33
Gian <gianluca.fiorinigmail.com>
2023-07-04 23:19:50 +0200

    Camera modes

--- Assets/LookAtCamera.cs
+++ Assets/LookAtCamera.cs
@@ -7,28 +7,38 @@ using UnityEngine;
 public class LookAtCamera : MonoBehaviour
 {
 
+    private Transform cameraTransform;
     public enum CameraMode
     {
         LookAt,
         InverseLookAt,
-        Forward
+        Forward,
+        Inverse
     }
 
     [SerializeField]
     private CameraMode _cameraMode;
-    
+
+    private void Start() {
+        cameraTransform = Camera.main.transform;
+    }
     private void LateUpdate() {
         switch (_cameraMode){
             case CameraMode.LookAt:
-                transform.LookAt(Camera.main.transform);                
+                transform.LookAt(cameraTransform);                
                 break;
             case CameraMode.InverseLookAt:
-                //gfiorini: capire
-                Vector3 dirFromCamera = transform.position - Camera.main.transform.position; 
-                transform.LookAt(transform.position + dirFromCamera);
+                // Calculate the direction from the object to the camera
+                Vector3 lookDirection = transform.position - cameraTransform.position;
+                // Rotate the object to look in the opposite direction of the camera
+                transform.LookAt(transform.position + lookDirection);
                 break;
             case CameraMode.Forward:
-                break;            
+                transform.forward = cameraTransform.forward;
+                break;
+            case CameraMode.Inverse:
+                transform.forward = -cameraTransform.forward;
+                break;                        
         }
         
     }
