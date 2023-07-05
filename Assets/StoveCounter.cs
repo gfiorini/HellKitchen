80ff27ac864aa219f0faf41b84504efbe471183c
Gian <gianluca.fiorinigmail.com>
2023-07-05 22:56:28 +0200

    cook meat

--- Assets/StoveCounter.cs
+++ Assets/StoveCounter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,16 +8,46 @@ public class StoveCounter : BaseCounter
 
     [SerializeField]
     private KitchenObjectRecipeSO[] recipes;
+
+    [SerializeField]
+    private float cookTime = 5f;
     
+    private float elapsedCookTime = 0f;
+
+    private bool isCooking = false;
+    private void Update() {
+        if (isCooking){
+            elapsedCookTime += Time.deltaTime;
+            if (elapsedCookTime > cookTime){
+                KitchenObjectRecipeSO cookedMeat = GetRecipe(GetKitchenObject().GetKitchenScriptableObject());
+                GetKitchenObject().DestroySelf();
+                AssignKitchenObject(cookedMeat.output, this);
+                StopCooking();
+            }
+        }
+        
+        
+    }
+
     public override void Interact(Player player) {
         if (GetKitchenObject() != null && player.GetKitchenObject() == null){
             GetKitchenObject().SetParent(player);
         } else if (GetKitchenObject() == null && player.GetKitchenObject() != null && ExistRecipe(player.GetKitchenObject())){
             player.GetKitchenObject().SetParent(this);
+            StartCooking();
         }
          
     }
+    private void StartCooking() {
+        elapsedCookTime = 0;
+        isCooking = true;
+    }
     
+    private void StopCooking() {
+        elapsedCookTime = 0;
+        isCooking = false;
+    }    
+
     private KitchenObjectRecipeSO GetRecipe(KitchenObjectSO input) {
         foreach (var ko in recipes){
             if (ko.input.objectName == input.objectName){
