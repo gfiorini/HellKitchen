737ea70e4f78ff4d21bd261324e4e9b049d1948b
gfiorini <gianluca.fiorini@gmail.com>
2023-07-10 15:01:23 +0200

    fix compilazione

---
+++ Assets/Scripts/Counter/IHasProgress.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+
+
+public interface IHasProgress 
+{
+    public event EventHandler<OnProgressChangeArgs> OnProgressChange;
+    public class OnProgressChangeArgs : EventArgs
+    {
+        public float progressNormalized;
+    }
+}
