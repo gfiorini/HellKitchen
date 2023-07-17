9dfd0236ec0b6488b1de03e012491e7de09e3db8
gfiorini <gianluca.fiorini@gmail.com>
2023-07-18 01:24:21 +0200

    refactor

--- Assets/Scripts/UI/RecipeUI.cs
+++ Assets/Scripts/UI/RecipeUI.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RecipeUI : MonoBehaviour
 {
@@ -24,8 +25,7 @@ public class RecipeUI : MonoBehaviour
 
         foreach (KitchenObjectSO recipeIngredient in recipe.ingredients){
             Transform iconImage = Instantiate(imageTemplate, imageContainer);
-            ImageTemplateUI imageTemplateUI = iconImage.GetComponent<ImageTemplateUI>();
-            imageTemplateUI.SetSprite(recipeIngredient.icon);
+            iconImage.GetComponent<Image>().sprite = recipeIngredient.icon;
             iconImage.gameObject.SetActive(true);
         }
         
